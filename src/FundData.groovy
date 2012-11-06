7e6a6ce705486ade38ef90663039fa1851087c7f
yaska80 <jaakko.sjoholm@gmail.com>
2012-11-06 18:21:03 +0200

    RegExp

--- src/FundData.groovy
+++ src/FundData.groovy
@@ -7,18 +7,19 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.2' )
 @GrabExclude('org.codehaus.groovy:groovy')
 import groovyx.net.http.HTTPBuilder
-import groovyx.net.http.Method
-
 import static groovyx.net.http.Method.GET
-import groovyx.net.http.ContentType
-
 import static groovyx.net.http.ContentType.TEXT
+
+@groovy.lang.Grab(group='joda-time', module='joda-time', version='2.1')
+import org.joda.time.LocalDate;
 
 class FundData {
     def httpAddress
     def name
     def startDate
     def data
+
+    static def pattern = ~/(\d{2})\.(\d{2})\.(\d{4});(\d+\.\d+)/
 
 
     FundData (httpAddress, name, startDate) {
@@ -46,6 +47,14 @@ class FundData {
     }
 
     def parseLine(line) {
+        def matcher = pattern.matcher(line)
 
+        matcher.find()
+        assert matcher.groupCount() == 4
+
+        def date = new LocalDate(matcher.group(1), matcher.group(2), matcher.group(3))
+        def value = Double.parseDouble(matcher.group(4))
+
+        [date, value]
     }
 }
