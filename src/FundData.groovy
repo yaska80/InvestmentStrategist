7125237311baea6f3d73ceb7f20c880f95d0a050
yaska80 <jaakko.sjoholm@gmail.com>
2012-11-06 16:58:07 +0200

    FundData moving along

--- src/FundData.groovy
+++ src/FundData.groovy
@@ -5,14 +5,22 @@
  */
 
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.2' )
+@GrabExclude('org.codehaus.groovy:groovy')
 import groovyx.net.http.HTTPBuilder
-import com.sun.org.apache.bcel.internal.generic.GETFIELD;
+import groovyx.net.http.Method
 
+import static groovyx.net.http.Method.GET
+import groovyx.net.http.ContentType
+
+import static groovyx.net.http.ContentType.TEXT
 
 class FundData {
     def httpAddress
     def name
     def startDate
+    def data
+
+
     FundData (httpAddress, name, startDate) {
         this.httpAddress = httpAddress
         this.name = name
@@ -21,7 +29,23 @@ class FundData {
 
     def init() {
         def http = new HTTPBuilder(httpAddress)
+        data = {}
 
-        http.request(GET, TEXT)
+        http.request(GET, TEXT) {
+            response.sucess { resp, reader ->
+                reader.eachLine { line ->
+                    def parsed = parseLine(line)
+                    data.put(parsed[0], parsed[1])
+                }
+            }
+
+            response.'404' { resp ->
+                throw new IllegalArgumentException("Couldn't get data from address ${httpAddress}")
+            }
+        }
+    }
+
+    def parseLine(line) {
+
     }
 }
